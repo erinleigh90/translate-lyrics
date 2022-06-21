1c787765b0636f542a2e091b297ff82f8e54dea8
Erin Beal <erinleig@amazon.com>
2022-06-20 17:15:14 -0700

    Adds sign in/sign out modal with first styles pass

---
+++ components/signIn.tsx
@@ -0,0 +1,70 @@
+import { Auth } from 'aws-amplify';
+import styles from '../styles/Home.module.css';
+
+export default function SignIn() {
+  const handleClose = () => {
+
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement> & { target: HTMLFormElement }) => {
+    event.preventDefault();
+
+    const form: FormData = new FormData(event.target);
+    if (form.get('username') && form.get('password')) {
+      const username: string = form.get('username') as string;
+      const password: string = form.get('password') as string;
+
+      try {
+        const { user } = await Auth.signUp({
+          username,
+          password
+        });
+        console.log(user);
+      } catch (error) {
+        console.log('error signing up:', error);
+      }
+    } else {
+
+    }
+  };
+  return (
+    <div>
+      <div className={styles.modalBackdrop}></div>
+      <div className={styles.modal} role="dialog">
+        <div className={styles.modalExit}>
+          <button aria-label="Close" type="button" class="_oda838">
+            <span>
+              <svg viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false">
+                <path d="m6 6 20 20"></path>
+                <path d="m26 6-20 20"></path>
+              </svg>
+            </span>
+          </button>
+        </div>
+        <header className={styles.modalHeader}>
+          <div>
+            <h3>Sign In or Sign Up</h3>
+          </div>
+        </header>
+        <div className={styles.modalBody}>
+          <form className={styles.signInForm} onSubmit={handleSubmit}>
+            <div className={styles.inputFields}>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                required />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                required />
+            </div>
+            <input className={styles.signInSubmit} type="submit" name="Sign In" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+
+}
--- components/siteHeader.tsx
+++ components/siteHeader.tsx
@@ -1,16 +1,28 @@
-import { Auth } from 'aws-amplify';
-
+import { useState } from "react";
 import styles from '../styles/Home.module.css';
+import SignIn from '../components/signIn';
 
 export default function SiteHeader() {
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    setShowSignIn(true);
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.headerDiv}>
-        <div className={styles.headerTitle}><h3>TranslateLyrics</h3></div>
-        <div className={styles.userMenuDiv}>
-          <input className={styles.button} type="submit" name="auth" value="Sign In/Sign Up" />
+    <div>
+      <header className={styles.header}>
+        <div className={styles.headerDiv}>
+          <div className={styles.headerTitle}>
+            <h3>TranslateLyrics</h3>
+          </div>
+          <div className={styles.userMenuDiv}>
+            <input className={styles.headerButton} type="submit" name="auth" value="Sign In/Sign Up" onClick={handleSubmit} />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showSignIn ? <SignIn /> : null}
+    </div>
   );
 }
