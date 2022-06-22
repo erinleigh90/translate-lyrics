79dcfc8cae44d398b8ad2cc77feef094f71d0737
Erin Beal <erinleig@amazon.com>
2022-06-22 11:58:13 -0700

    Adds authentication check to new song page, makes subheader pretty

--- pages/addSong.tsx
+++ pages/addSong.tsx
@@ -1,9 +1,19 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../utilities/userContextMethods";
 import styles from '../styles/Home.module.css';
 
 export default function AddSong() {
+  const user = useContext(UserContext);
+  const authenticated = (user != null);
+
   return (
-    <div className={styles.card}>
-      <h2>New Song</h2>
+    <div>
+      {(authenticated) ?
+        <div className={styles.card}>
+          <h2>New Song</h2>
+        </div>
+        :
+        <div>Oops! Please log in to add a song!</div>}
     </div>
   );
 }
