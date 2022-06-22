12a98bf9d92a35074fcb42aa9ab738207614a850
Erin Beal <erinleig@amazon.com>
2022-06-22 14:29:36 -0700

    Turn song cards on home page into links

--- components/songCard.tsx
+++ components/songCard.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function SongCard({ song }: any) {
 
   return (
-    <div className={styles.card} key={song.id}>
-      <h3>{song.title}</h3>
-      <div className={styles.lyricsDiv}>{song.lyrics}</div>
-      <div>...</div>
-    </div>
+    <Link href={`/songs/${song.id}`}>
+      <div className={`${styles.card} ${styles.songCard}`} key={song.id}>
+        <h3>{song.title}</h3>
+        <div className={styles.lyricsDiv}>{song.lyrics}</div>
+        <div>...</div>
+      </div>
+    </Link>
   );
 }
