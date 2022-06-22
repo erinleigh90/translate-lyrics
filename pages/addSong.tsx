9177a8556eaae8ef4ce3681d8fa4932ff0e5092d
Erin Beal <erinleig@amazon.com>
2022-06-22 12:13:46 -0700

    Adds form with song info, fixes some style incompatibilities

--- pages/addSong.tsx
+++ pages/addSong.tsx
@@ -6,14 +6,21 @@ export default function AddSong() {
   const user = useContext(UserContext);
   const authenticated = (user != null);
 
+  const newSongCard = (
+    <div className={styles.card}>
+      <h2>New Song</h2>
+      <form className={styles.inputFields}>
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="artist" placeholder="Artist" />
+        <input type="text" name="album" placeholder="Album" />
+        <input type="text" name="lyrics" placeholder="Lyrics" />
+      </form>
+    </div>
+  );
+
   return (
     <div>
-      {(authenticated) ?
-        <div className={styles.card}>
-          <h2>New Song</h2>
-        </div>
-        :
-        <div>Oops! Please log in to add a song!</div>}
+      {(authenticated) ? newSongCard : <div>Oops! Please log in to add a song!</div>}
     </div>
   );
 }
