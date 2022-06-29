f8842ced37deec4bfee8fafb176e39d4df967aac
Erin Beal <erinleig@amazon.com>
2022-06-28 18:07:14 -0700

    Adds utility ts file for CRUD calls to API

---
+++ utilities/databaseCrudMethods.ts
@@ -0,0 +1,63 @@
+import { API } from 'aws-amplify';
+import { createArtist, createAlbum, createSong } from '../src/graphql/mutations';
+
+export async function insertArtist(name: string) {
+  try {
+    const { data }: any = await API.graphql({
+      authMode: 'AMAZON_COGNITO_USER_POOLS',
+      query: createArtist,
+      variables: {
+        input: {
+          name: name
+        }
+      }
+    });
+
+    return data.createArtist;
+  } catch(e: any) {
+    console.error(...e.errors);
+    throw new Error(e.errors[0].message);
+  }
+}
+
+export async function insertAlbum(title: string, artistId: string | null = null) {
+  try {
+    const { data }: any = await API.graphql({
+      authMode: 'AMAZON_COGNITO_USER_POOLS',
+      query: createAlbum,
+      variables: {
+        input: {
+          title: title,
+          artistId: artistId
+        }
+      }
+    });
+
+    return data.createSong;
+  } catch(e: any) {
+    console.error(...e.errors);
+    throw new Error(e.errors[0].message);
+  }
+}
+
+export async function insertSong(title: string, lyrics: string, artist: any = null, album: any = null) {
+  try {
+    const { data }: any = await API.graphql({
+      authMode: 'AMAZON_COGNITO_USER_POOLS',
+      query: createSong,
+      variables: {
+        input: {
+          title: title,
+          lyrics: lyrics,
+          artistId: artist.id,
+          albumId: album.id
+        }
+      }
+    });
+
+    return data.createSong;
+  } catch(e: any) {
+    console.error(...e.errors);
+    throw new Error(e.errors[0].message);
+  }
+}
