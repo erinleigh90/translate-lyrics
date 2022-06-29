eb2aea68f337c4b0ecda7586cdcc9b4dd114946d
Erin Beal <erinleig@amazon.com>
2022-06-28 18:08:30 -0700

    Simplifies home page return of html

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { listSongs } from '../src/graphql/queries';
 import SongCard from '../components/songCardLink';
 
 export async function getServerSideProps({ req }: any) {
-  const SSR = withSSRContext({ req })
+  const SSR = withSSRContext({ req });
   const { data } = await SSR.API.graphql({ query: listSongs });
 
   return {
@@ -14,19 +14,12 @@ export async function getServerSideProps({ req }: any) {
 }
 
 export default function Home({ songs }: any) {
-  const getSongComponents = () => {
-    let songCards = [];
-    for (let song of songs) {
-      songCards.push(SongCard({ song }));
-    }
-    return <div>{songCards}</div>;
-  }
+  if(songs && songs.length > 0) {
+    const songCards = songs.map((song: any) => <SongCard song={song} key={song.id}/>);
 
+    return <div>{songCards}</div>
+  }
   return (
-    <div>
-      {(songs && songs.length > 0) ?
-        getSongComponents()
-        : <div>No Songs!</div>}
-    </div>
+    <div>No Songs!</div>
   );
 }
