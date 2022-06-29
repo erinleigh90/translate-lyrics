4164a11f5bad90d6387b8c665ebc4300df3e77da
Erin Beal <erinleig@amazon.com>
2022-06-29 15:26:38 -0700

    Changes home page query to DataStore query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,25 @@
 import { withSSRContext } from 'aws-amplify';
-import { listSongs } from '../src/graphql/queries';
+import { serializeModel } from '@aws-amplify/datastore/ssr';
+import { Song } from '../src/models/index';
 import SongCard from '../components/songCardLink';
 import styles from '../styles/Home.module.css';
 
 export async function getServerSideProps({ req }: any) {
   const SSR = withSSRContext({ req });
-  const { data } = await SSR.API.graphql({ query: listSongs });
+  const songs = await SSR.DataStore.query(Song);
 
   return {
     props: {
-      songs: data.listSongs.items
+      songs: serializeModel(songs)
     }
   }
 }
 
-export default function Home({ songs }: any) {
+export default function Home({ songs }: {songs:[Song]}) {
   if(songs && songs.length > 0) {
-    const songCards = songs.map((song: any) => <SongCard song={song} key={song.id}/>);
+    const songCards = songs.map((song: Song) => <SongCard song={song} key={song.id}/>);
 
-    return <div className={styles.main}>{songCards}</div>
+    return <div className={styles.main}>{songCards}</div>;
   }
   return (
     <div className={styles.main}>No Songs!</div>
